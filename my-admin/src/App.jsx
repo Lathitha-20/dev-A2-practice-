2df92520c883dbf44dec419416c9f8290503426f
Lathitha-20 <lathiesiyekwana@gmail.com>
2025-11-09 18:00:36 +0000

    yarn the application

--- my-admin/src/App.jsx
+++ my-admin/src/App.jsx
@@ -1,17 +1,16 @@
 import { Admin, CustomRoutes } from "react-admin";
 import { Route } from "react-router-dom";
-import { createTrailbaseProvider } from "./dataProvider";
+import { createTrailbaseProvider } from "./ra-trailbase";
 import { CreateUser, LoginPage } from "./create_user";
 
 const TRAILBASE_URL = "http://fantastic-rotary-phone-wr9q57gqqvjr3grq9-4000.app.github.dev/";
-const { dataProvider, authProvider } = await createTrailbaseProvider(TRAILBASE_URL);
+const { dataProvider } = await createTrailbaseProvider(TRAILBASE_URL);
 
 function App() {
   return (
     <Admin
       loginPage={LoginPage}
       dataProvider={dataProvider}
-      authProvider={authProvider}
     >
       <CustomRoutes noLayout>
         <Route path="/create-user" element={<CreateUser />} />
